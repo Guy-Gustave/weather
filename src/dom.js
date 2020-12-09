3581c9397842a32864f55774ca9f8adf5a5a50b5
Guy <guymoustapha@gmail.com>
2020-12-09 11:15:08 +0200

    add html skeleton, css and dom file

---
+++ src/dom.js
@@ -0,0 +1,19 @@
+
+const dom = {
+  locally : document.querySelector('[locally]'),
+  date : document.querySelector('[date]'),
+  inputForm: document.querySelector('[form-inline]'),
+  inputValue: document.querySelector('[formInput]'),
+  submit : document.querySelector('[submitWeather]'),
+  city: document.querySelector('[.data-name]'),
+  icon: document.querySelector('[.data-icon]'),
+  temp: document.querySelector('.temp'),
+  status: document.querySelector('[.data-status]'),
+  humidity: document.querySelector('[.data-humid]'),
+  feelsLike: document.querySelector('[.data-feels-like]'),
+  date: document.querySelector('[.data-date]'),
+  celcius: document.querySelector('[data-celcius]'),
+  fahren: document.querySelector('[.data-fahren]'),
+};
+
+export default { dom };
